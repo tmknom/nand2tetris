478a6fa93aa1b4fc6fb3b07050ad0781d1e52d9c
tmknom <tmk.nom@gmail.com>
2021-01-16 10:44:08 +0900

    乗算プログラムの実装：初期値の設定

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//
+// 初期値の設定
+//
+
+// R0(=RAM[0])に初期値「3」をセット
+@3 // Aレジスタに定数「3」をセット
+D=A // Aレジスタの値(=3)をDレジスタに格納
+@0 // Aレジスタにアドレス0をセット
+M=D // RAM[A]=RAM[0]にDレジスタの値をセット
+
+// R1に初期値「2」をセット
+@2 // Aレジスタに定数「2」をセット
+D=A // Aレジスタ(=2)の値をDレジスタに格納
+@1 // Aレジスタにアドレス1をセット
+M=D // RAM[A]=RAM[1]にDレジスタの値をセット
+
+(LOOP)
+    @END
+    D;JEQ // if D=0 goto END
+
+(END)
+    @END
+    0;JMP
