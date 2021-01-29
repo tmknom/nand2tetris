7a65aea177354d4c4bb8c49c2a129db91a7e22e7
tmknom <tmk.nom@gmail.com>
2021-01-30 08:34:07 +0900

    ExpressionListの実装

--- 10/parsing/expression.go
+++ 10/parsing/expression.go
@@ -2,6 +2,64 @@ package parsing
 
 import "../token"
 
+type ExpressionList struct {
+	First               *Expression
+	CommaAndExpressions []*CommaAndExpression
+}
+
+func NewExpressionList() *ExpressionList {
+	return &ExpressionList{
+		CommaAndExpressions: []*CommaAndExpression{},
+	}
+}
+
+func (e *ExpressionList) AddExpression(expression *Expression) error {
+	if err := expression.Check(); err != nil {
+		return err
+	}
+
+	if e.First == nil {
+		e.First = expression
+	} else {
+		e.CommaAndExpressions = append(e.CommaAndExpressions, NewCommaAndExpression(expression))
+	}
+	return nil
+}
+
+func (e *ExpressionList) ToXML() []string {
+	result := []string{}
+	result = append(result, "<expressionList>")
+
+	if e.First != nil {
+		result = append(result, e.First.ToXML()...)
+	}
+
+	for _, item := range e.CommaAndExpressions {
+		result = append(result, item.ToXML()...)
+	}
+	result = append(result, "</expressionList>")
+	return result
+}
+
+type CommaAndExpression struct {
+	*Comma
+	*Expression
+}
+
+func NewCommaAndExpression(expression *Expression) *CommaAndExpression {
+	return &CommaAndExpression{
+		Comma:      ConstComma,
+		Expression: expression,
+	}
+}
+
+func (c *CommaAndExpression) ToXML() []string {
+	result := []string{}
+	result = append(result, c.Comma.ToXML())
+	result = append(result, c.Expression.ToXML()...)
+	return result
+}
+
 type Expression struct {
 	*token.Token
 }
@@ -15,4 +73,8 @@ func NewExpression(token *token.Token) *Expression {
 func (e *Expression) Check() error {
 	// TODO Expression実装時にちゃんと書く
 	return nil
+}
+
+func (e *Expression) ToXML() []string {
+	return []string{e.Token.ToXML()}
 }
