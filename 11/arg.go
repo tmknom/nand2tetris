2ec5abc1a36c308b44f58b6bd4721c6875a8826b
tmknom <tmk.nom@gmail.com>
2021-02-02 09:51:51 +0900

    コンパイル対象のディレクトリをSymbolTableに変更

--- 11/arg.go
+++ 11/arg.go
@@ -11,7 +11,7 @@ type Arg struct {
 	files []string
 }
 
-const DefaultArg = "Fixture/ExpressionLessSquare/Main.jack"
+const DefaultArg = "Fixture/SymbolTable/"
 
 func NewArg(args []string) *Arg {
 	arg := DefaultArg
